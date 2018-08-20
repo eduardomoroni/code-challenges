542bd92488ea28ad90ca29d773992f3af66bcc17
Eduardo Moroni <eduardomoroni@gmail.com>
2018-08-20 18:25:23 -0300

    add js playground

---
+++ codewars/js-playground/src/index.js
@@ -0,0 +1,13 @@
+const someFunction = () => {
+  return 'bar';
+};
+
+const someAsyncFunction = async (fetch) => {
+  return fetch('someUrl');
+};
+
+
+module.exports = {
+  someFunction,
+  someAsyncFunction,
+};
