be8ff4cb944668269372d26024a4ef5bebd1f9be
Eduardo Moroni <eduardomoroni@gmail.com>
2018-09-25 09:09:21 -0300

    feat: finish radio buttons styles

---
+++ frontend/frontloops/src/modules/markup/Loop-01/Step-02/PaymentMethodForm.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import "./PaymentMethodForm.css";
+
+const Method = ({ img, label }) => (
+  <Fragment>
+    <input
+      className="payment-form__input"
+      id={label}
+      type="radio"
+      name="payment-radio"
+    />
+    <label
+      className="payment-form__input-label"
+      htmlFor={label}
+      id={`${label}-label`}
+    >
+      {label}
+      <span className="checkmark" />
+    </label>
+  </Fragment>
+);
+
+export class PaymentMethodForm extends React.PureComponent {
+  static Method = Method;
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <form className="payment-form">
+        {React.Children.map(children, child => <Method {...child.props} />)}
+      </form>
+    );
+  }
+}
--- frontend/frontloops/src/modules/markup/Loop-01/Step-02/StepTwo.jsx
+++ frontend/frontloops/src/modules/markup/Loop-01/Step-02/StepTwo.jsx
@@ -3,13 +3,25 @@ import React, { Component } from "react";
 import { CenteredFrame } from "../../../layout/CenteredFrame";
 import { Dots } from "./Dots";
 import "./StepTwo.css";
+import {
+  PaymentMethodForm,
+  PaymentMethodForm as Payment
+} from "./PaymentMethodForm";
+import VisaImg from "./img/visa.svg";
+import MasterCardImg from "./img/mastercard.svg";
+import MaestroImg from "./img/maestro.svg";
 
 export class StepTwo extends Component {
   render() {
     return (
       <CenteredFrame>
         <Dots selected={1} quantity={3} />
-        <h2>Payment Method</h2>
+        <h2>Payment method</h2>
+        <PaymentMethodForm>
+          <Payment.Method label="Visa" img={VisaImg} />
+          <Payment.Method label="MasterCard" img={MasterCardImg} />
+          <Payment.Method label="Maestro" img={MaestroImg} />
+        </PaymentMethodForm>
       </CenteredFrame>
     );
   }
